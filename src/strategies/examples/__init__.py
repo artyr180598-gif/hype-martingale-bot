cc88be32b873fe0dac1bf45d8d143cecf2143e80
artyr180598-gif <artyr180598@gmail.com>
2026-09-24 16:42:48 +0200

    Transplant HyperData strategy examples init

---
+++ src/strategies/examples/__init__.py
@@ -0,0 +1,6 @@
+from .cvd_momentum import CVDMomentum
+from .funding_rate_arb import FundingRateArb
+from .liquidation_cascade import LiquidationCascade
+from .whale_follow import WhaleFollow
+
+__all__ = ["CVDMomentum", "FundingRateArb", "LiquidationCascade", "WhaleFollow"]
